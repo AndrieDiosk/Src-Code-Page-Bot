7f2ed43cb94d0f01c50b6265bcde8e2f1e28fbd6
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2025-03-29 19:15:39 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,48 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'help',
   description: 'Show available commands',
-  usage: 'help [ command name ]',
   author: 'System',
-  execute(senderId, args, pageAccessToken) {
+  execute(senderId, args, pageAccessToken, sendMessage) {
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
-    if (args.length > 0) {
-      const commandName = args[0].toLowerCase();
-      const commandFile = commandFiles.find(file => {
-        const command = require(path.join(commandsDir, file));
-        return command.name.toLowerCase() === commandName;
-      });
-
-      if (commandFile) {
-        const command = require(path.join(commandsDir, commandFile));
-        const commandDetails = `Command name : ${command.name}
-Description : ${command.description}
-Usage : ${command.usage}`;
-        
-        sendMessage(senderId, { text: commandDetails }, pageAccessToken);
-      } else {
-        sendMessage(senderId, { text: `Command "${commandName}" not found.` }, pageAccessToken);
-      }
-      return;
-    }
-
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
-      return `${command.name}`;
+      return `⟿ ${command.name}\n  - ${command.description}\n  - Credits: ${command.author}`;
     });
 
-    const helpMessage = `Available Commands :
-
-${commands.join('\n')}
-
-Chat help [ name ] 
-to see command details.`;
-
+    const totalCommands = commandFiles.length;
+    const helpMessage = `Here are the available commands: \nTotal commands: ${totalCommands} \n\n${commands.join('\n\n')}`;
+    
     sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
