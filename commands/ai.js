8ff6348a94148e072294c374b25560de812dc52b
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2025-05-07 12:45:04 +0800

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -9,7 +9,7 @@ module.exports = {
     try {
       const apiUrl = `https://jonell01-ccprojectsapihshs.hf.space/api/blackb?ask=${encodeURIComponent(prompt)}&id=${senderId}`;
       const respo = await axios.get(apiUrl);
-      const text = respo.data.response;
+      const text = respo.data.message;
 
       // Split the response into chunks if it exceeds 2000 characters
       const maxMessageLength = 2000;
